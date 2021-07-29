05dabf163148303d81bceed3d51ce5b8f0c500e8
moacyrrln <69820961+moacyrrln@users.noreply.github.com>
2021-07-29 00:43:25 -0300

    Add files via upload

---
+++ Urna Eletronica/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace Urna_Eletronica
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button0 = new System.Windows.Forms.Button();
+			this.buttonConfirma = new System.Windows.Forms.Button();
+			this.buttonCorrige = new System.Windows.Forms.Button();
+			this.buttonBranco = new System.Windows.Forms.Button();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.lblData = new System.Windows.Forms.ToolStripStatusLabel();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.Data_hora = new System.Windows.Forms.Label();
+			this.campo1 = new System.Windows.Forms.Label();
+			this.campo2 = new System.Windows.Forms.Label();
+			this.campo3 = new System.Windows.Forms.Label();
+			this.campo4 = new System.Windows.Forms.Label();
+			this.campo5 = new System.Windows.Forms.Label();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(693, 46);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(42, 35);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(741, 46);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(42, 35);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(789, 46);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(42, 35);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(693, 87);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(42, 35);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(789, 128);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(42, 35);
+			this.button9.TabIndex = 8;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(741, 128);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(42, 35);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(693, 128);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(42, 35);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button0
+			// 
+			this.button0.Location = new System.Drawing.Point(741, 169);
+			this.button0.Name = "button0";
+			this.button0.Size = new System.Drawing.Size(42, 35);
+			this.button0.TabIndex = 9;
+			this.button0.Text = "0";
+			this.button0.UseVisualStyleBackColor = true;
+			this.button0.Click += new System.EventHandler(this.button0_Click);
+			// 
+			// buttonConfirma
+			// 
+			this.buttonConfirma.Location = new System.Drawing.Point(804, 210);
+			this.buttonConfirma.Name = "buttonConfirma";
+			this.buttonConfirma.Size = new System.Drawing.Size(77, 35);
+			this.buttonConfirma.TabIndex = 12;
+			this.buttonConfirma.Text = "CONFIRMA";
+			this.buttonConfirma.UseVisualStyleBackColor = true;
+			// 
+			// buttonCorrige
+			// 
+			this.buttonCorrige.Location = new System.Drawing.Point(727, 210);
+			this.buttonCorrige.Name = "buttonCorrige";
+			this.buttonCorrige.Size = new System.Drawing.Size(71, 35);
+			this.buttonCorrige.TabIndex = 11;
+			this.buttonCorrige.Text = "CORRIGE";
+			this.buttonCorrige.UseVisualStyleBackColor = true;
+			this.buttonCorrige.Click += new System.EventHandler(this.buttonCorrige_Click);
+			// 
+			// buttonBranco
+			// 
+			this.buttonBranco.Location = new System.Drawing.Point(655, 210);
+			this.buttonBranco.Name = "buttonBranco";
+			this.buttonBranco.Size = new System.Drawing.Size(66, 35);
+			this.buttonBranco.TabIndex = 10;
+			this.buttonBranco.Text = "BRANCO";
+			this.buttonBranco.UseVisualStyleBackColor = true;
+			this.buttonBranco.Click += new System.EventHandler(this.buttonBranco_Click);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblData});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 285);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(907, 22);
+			this.statusStrip1.TabIndex = 13;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// lblData
+			// 
+			this.lblData.Name = "lblData";
+			this.lblData.Size = new System.Drawing.Size(16, 17);
+			this.lblData.Text = "...";
+			this.lblData.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Data_hora
+			// 
+			this.Data_hora.AutoSize = true;
+			this.Data_hora.Location = new System.Drawing.Point(12, 9);
+			this.Data_hora.Name = "Data_hora";
+			this.Data_hora.Size = new System.Drawing.Size(60, 15);
+			this.Data_hora.TabIndex = 14;
+			this.Data_hora.Text = "Data_hora";
+			// 
+			// campo1
+			// 
+			this.campo1.AutoSize = true;
+			this.campo1.BackColor = System.Drawing.Color.Transparent;
+			this.campo1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.campo1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.campo1.Location = new System.Drawing.Point(12, 87);
+			this.campo1.MinimumSize = new System.Drawing.Size(15, 17);
+			this.campo1.Name = "campo1";
+			this.campo1.Size = new System.Drawing.Size(15, 17);
+			this.campo1.TabIndex = 15;
+			// 
+			// campo2
+			// 
+			this.campo2.AutoSize = true;
+			this.campo2.BackColor = System.Drawing.Color.Transparent;
+			this.campo2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.campo2.Location = new System.Drawing.Point(33, 87);
+			this.campo2.MinimumSize = new System.Drawing.Size(15, 17);
+			this.campo2.Name = "campo2";
+			this.campo2.Size = new System.Drawing.Size(15, 17);
+			this.campo2.TabIndex = 16;
+			// 
+			// campo3
+			// 
+			this.campo3.AutoSize = true;
+			this.campo3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.campo3.Location = new System.Drawing.Point(54, 87);
+			this.campo3.MinimumSize = new System.Drawing.Size(15, 17);
+			this.campo3.Name = "campo3";
+			this.campo3.Size = new System.Drawing.Size(15, 17);
+			this.campo3.TabIndex = 17;
+			// 
+			// campo4
+			// 
+			this.campo4.AutoSize = true;
+			this.campo4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.campo4.Location = new System.Drawing.Point(75, 87);
+			this.campo4.MinimumSize = new System.Drawing.Size(15, 17);
+			this.campo4.Name = "campo4";
+			this.campo4.Size = new System.Drawing.Size(15, 17);
+			this.campo4.TabIndex = 18;
+			// 
+			// campo5
+			// 
+			this.campo5.AutoSize = true;
+			this.campo5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.campo5.Location = new System.Drawing.Point(96, 87);
+			this.campo5.MinimumSize = new System.Drawing.Size(15, 17);
+			this.campo5.Name = "campo5";
+			this.campo5.Size = new System.Drawing.Size(15, 17);
+			this.campo5.TabIndex = 19;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(789, 87);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(42, 35);
+			this.button6.TabIndex = 20;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(741, 87);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(42, 35);
+			this.button5.TabIndex = 21;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// timer2
+			// 
+			this.timer2.Enabled = true;
+			this.timer2.Interval = 250;
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(907, 307);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.campo5);
+			this.Controls.Add(this.campo4);
+			this.Controls.Add(this.campo3);
+			this.Controls.Add(this.campo2);
+			this.Controls.Add(this.campo1);
+			this.Controls.Add(this.Data_hora);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.buttonConfirma);
+			this.Controls.Add(this.buttonCorrige);
+			this.Controls.Add(this.buttonBranco);
+			this.Controls.Add(this.button0);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.MinimumSize = new System.Drawing.Size(16, 39);
+			this.Name = "Form1";
+			this.Text = "Simulador Urna Eletrônica XP";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button0;
+		private System.Windows.Forms.Button buttonConfirma;
+		private System.Windows.Forms.Button buttonCorrige;
+		private System.Windows.Forms.Button buttonBranco;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel lblData;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label Data_hora;
+		private System.Windows.Forms.Label campo1;
+		private System.Windows.Forms.Label campo2;
+		private System.Windows.Forms.Label campo3;
+		private System.Windows.Forms.Label campo4;
+		private System.Windows.Forms.Label campo5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Timer timer2;
+	}
+}
+
---
+++ Urna Eletronica/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urna_Eletronica
+{
+	public partial class Form1 : Form
+	{
+		string codigo_candidato = "";
+		char[] campo = new char[5];
+		int intConta = 0;
+
+
+
+		private void atualiza_codigo_candidato()
+		{
+			char[] arr = new char[5];
+			arr = codigo_candidato.ToCharArray(0, codigo_candidato.Length);
+
+
+			if (codigo_candidato.Length >= 1)
+			{
+				campo1.Text = arr[0].ToString();
+			}
+			if (codigo_candidato.Length >= 2)
+			{
+				campo2.Text = arr[1].ToString();
+			}
+			if (codigo_candidato.Length >= 3)
+			{
+				campo3.Text = arr[2].ToString();
+			}
+			if (codigo_candidato.Length >= 4)
+			{
+				campo4.Text = arr[3].ToString();
+			}
+			if (codigo_candidato.Length >= 5)
+			{
+				campo5.Text = arr[4].ToString();
+			}
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 1);
+			atualiza_codigo_candidato();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 2);
+			atualiza_codigo_candidato();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 3);
+			atualiza_codigo_candidato();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 4);
+			atualiza_codigo_candidato();
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 5);
+			atualiza_codigo_candidato();
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 6);
+			atualiza_codigo_candidato();
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 7);
+			atualiza_codigo_candidato();
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 8);
+			atualiza_codigo_candidato();
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 9);
+			atualiza_codigo_candidato();
+		}
+
+		private void button0_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = String.Concat(codigo_candidato, 0);
+			atualiza_codigo_candidato();
+		}
+
+		private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			Data_hora.Text = DateTime.Now.ToString();
+			
+		}
+
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+			if (intConta == 0)
+			{
+				campo1.BackColor = Color.Transparent;
+				campo2.BackColor = Color.Transparent;
+				campo3.BackColor = Color.Transparent;
+				campo4.BackColor = Color.Transparent;
+				campo5.BackColor = Color.Transparent;
+			}
+
+			if (intConta == 1)
+			{
+				if (codigo_candidato.Length == 0) campo1.BackColor = Color.FromArgb(192, 255, 255);
+				if (codigo_candidato.Length == 1) campo2.BackColor = Color.FromArgb(192, 255, 255);
+				if (codigo_candidato.Length == 2) campo3.BackColor = Color.FromArgb(192, 255, 255);
+				if (codigo_candidato.Length == 3) campo4.BackColor = Color.FromArgb(192, 255, 255);
+				if (codigo_candidato.Length == 4) campo5.BackColor = Color.FromArgb(192, 255, 255);
+			}
+
+			intConta += 1;
+			if (intConta > 1)
+				intConta = 0;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+
+		private void buttonBranco_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void buttonCorrige_Click(object sender, EventArgs e)
+		{
+			codigo_candidato = "";
+			campo1.Text = "";
+			campo2.Text = "";
+			campo3.Text = "";
+			campo4.Text = "";
+			campo5.Text = "";
+		}
+
+		
+	}
+}
